b02b1630e623353bc6f5414465866bbffd8c83a4
박채빈 <coqls0219@korea-id.co.kr>
2025-11-13 13:55:11 +0900

    Update to v0.1.1 with hyphens and gbp alias

--- Formula/gradle-build-profiler.rb
+++ Formula/gradle-build-profiler.rb
@@ -1,19 +1,20 @@
-# homebrew-tap/Formula/gradle-build-profiler.rb에 복사하세요
-
 class GradleBuildProfiler < Formula
   desc "Analyze Gradle build profiles and provide performance insights"
   homepage "https://github.com/Chaebin-Park/GradleBuildProfiler"
-  url "https://github.com/Chaebin-Park/GradleBuildProfiler/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "7f9ecbba30206febe2c482951f11bc5e8a68d27f8b899988bcb17366ed3631f7"
+  url "https://github.com/Chaebin-Park/GradleBuildProfiler/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "4cbb367053ac1206568b444ae56ddb0302cc5d252f68aa1d4bf34ea30bd499bb"
   license "MIT"
 
   depends_on "rust" => :build
 
   def install
     system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+    # 짧은 별칭 추가
+    bin.install_symlink "gradle-build-profiler" => "gbp"
   end
 
   test do
-    assert_match "gradle-profiler", shell_output("#{bin}/gradle_build_profiler --help")
+    assert_match "gradle-profiler", shell_output("#{bin}/gradle-build-profiler --help")
+    assert_match "gradle-profiler", shell_output("#{bin}/gbp --help")
   end
 end
