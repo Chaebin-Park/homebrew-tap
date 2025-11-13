94885bcca2c01cc70cb86f2cdc7744bcfecce9ba
박채빈 <coqls0219@korea-id.co.kr>
2025-11-13 15:59:51 +0900

    sha 수정

--- Formula/gradle-dependency-analyzer.rb
+++ Formula/gradle-dependency-analyzer.rb
@@ -2,7 +2,7 @@ class GradleDependencyAnalyzer < Formula
   desc "Analyze and visualize module dependencies in Android/Gradle projects"
   homepage "https://github.com/Chaebin-Park/gradle-dependency-analyzer"
   url "https://github.com/Chaebin-Park/gradle-dependency-analyzer/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "PLACEHOLDER_SHA256"
+  sha256 "efca4aad95c3d38b60c7d005a4e1f703c0787f0cee6a2f840508ddc320f4cdb8"
   license "MIT"
 
   depends_on "rust" => :build
